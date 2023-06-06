c286c48bb87a291f46ed0b68e85bc47a10218386
Daniel Cuevas <ivandaniel1224@gmail.com>
2023-06-06 08:55:34 -0500

    Main styles

--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@ const closeMenu = document.querySelector(".close-menu");
 const menuList = document.querySelector(".header__nav");
 const menuListItems = [...document.querySelectorAll(".header__nav-list li")];
 
-burgerMenu.addEventListener("click", () => {
+function openMenu() {
   menuList.classList.toggle("hidden");
   closeMenu.classList.remove("hidden");
   burgerMenu.classList.add("hidden");
-});
-
-closeMenu.addEventListener("click", () => {
+}
+function hiddenMenu() {
   menuList.classList.add("hidden");
   closeMenu.classList.add("hidden");
   burgerMenu.classList.remove("hidden");
-});
+}
 
+burgerMenu.addEventListener("click", openMenu);
+closeMenu.addEventListener("click", hiddenMenu);
 menuListItems.forEach((element) => {
   element.addEventListener("click", () => {
-    menuList.classList.add("hidden");
-    closeMenu.classList.add("hidden");
-    burgerMenu.classList.remove("hidden");
+    hiddenMenu();
   });
 });
